b1f4a21da7cffc63873fb9b89e3ec92be98aa516
Sanjeev Rai <146112569+SanjeevRai07@users.noreply.github.com>
2025-09-05 23:33:34 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,12 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a = 10;
+
+        int b = 5;
+        int mul = a * b;
+        int diff = a - b;
+
+        System.out.println(mul);
+    }
+    
+}
