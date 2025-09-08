b3c2999a5e496e18ade668480c0e8f0f114a0f56
Sanjeev Rai <146112569+SanjeevRai07@users.noreply.github.com>
2025-09-08 23:54:51 +0530

    Add files via upload

---
+++ Add_function.java
@@ -0,0 +1,11 @@
+public class Add_function {
+    // method
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int result = add(10, 20);  // method call
+        System.out.println("Sum: " + result);
+    }
+}
